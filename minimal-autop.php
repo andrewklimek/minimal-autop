b352f80133618f171b929ce67d63cc5c6e952045
Andrew <andrew@readycat.net>
2016-09-24 03:58:50 +0000

    don’t add anything to <pre> content

--- minimal-autop.php
+++ minimal-autop.php
@@ -3,7 +3,7 @@
 Plugin Name: Minimal AutoP
 Plugin URI:  https://github.com/andrewklimek/minimal-autop/
 Description: very simple replacement for wpautop, won't interfere with your html.
-Version:     0.1.1
+Version:     0.1.2
 Author:      Andrew J Klimek
 Author URI:  https://readycat.net
 License:     GPL2
@@ -22,15 +22,44 @@ Minimal AutoP. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 function mnml_autop( $c ) {
-	$c = "\n\n{$c}\n\n";// pad first and last line
-	$c = str_replace( array( "\r\n", "\r" ), "\n", $c );// standardize line breaks
+	
+	// not sure if we need to standardize line breaks. wp_autop does.
+	// Maybe if entries were made on a different OS than current server?
+	// And would Darwin \r ever still show up??
+	$c = str_replace( array( "\r\n", "\r" ), "\n", $c, $count );
+	// How often does this happen?
+	if ( $count ) error_log( 'replaced \r\n or \r');
+	
+	$have_pre = false;
+	
+	if ( false !== strpos( $c, "<pre") ) {// check for pre tags. could use stripos but is it slower? this plugin is just for me
+	
+		$have_pre = true;
+	
+		$c = preg_replace_callback(
+			'|<pre[\s\S]+?</pre>|',
+			function ($matches) {
+				return str_replace( "\n", "~crazyNewLinePlaceholder~", $matches[0]);
+			},
+			$c
+		);
+	}
+	
 	$c = str_replace( array( "\n<br>", "<br>\n" ), "<br>", $c );// trim line breaks from any <br>s they might code
-	$c = preg_replace( "/(\n|\]|>)\n+([^\n\[<])/", "$1\n<p>$2", $c );// opening <p>
-	// $c = preg_replace( "/([^\n\]>])\n+?(\n|\[|<)/", "$1</p>\n$2", $c );// closing </p>
-	$c = preg_replace( "/([^\n\]>])\n([^\n\[<])/", "$1\n<br>$2", $c );// <br>
+
+	// I tk \n out of the first group, wasn't sure why it was there.
+	$c = preg_replace( "|([\n>\]])\n+([^\n<\[])|", "$1\n<p>$2", $c );// opening <p>
+	
+	// $c = preg_replace( "/([^\n\]>])\n+?(\n|\[|<)/", "$1</p>\n$2", $c );// closing </p> isnt technically needed
+	$c = preg_replace( "|([^\n\]>])\n([^\n\[<])|", "$1\n<br>$2", $c );// <br>
+	
+	if ( $have_pre ) {
+		$c = str_replace( "~crazyNewLinePlaceholder~", PHP_EOL, $c);
+	}
 	
 	//Let's try to keep double spacing!
 	$c = str_replace( ".  ", ".&nbsp; ", $c );
+	
 	return $c;
 }
 
